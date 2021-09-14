2bb7ffb7cb83bcf97636d96724395c1cd47f8e62
koopa1338 <sinner1991@gmail.com>
2021-09-14 21:54:03 +0200

    setup logging

--- mugen-server/src/main.rs
+++ mugen-server/src/main.rs
@@ -11,13 +11,53 @@ mod error;
 mod models;
 mod services;
 
+use chrono::Local;
 use config::app;
 use dotenv::dotenv;
-use rocket::{Build, Rocket};
+use fern::{log_file, Dispatch};
+use log::LevelFilter;
+use rocket::{Build, Error as RocketError, Rocket};
+
+fn setup_logging() {
+    //TODO: Errorhandling to remove unwraps
+    let base_logging = Dispatch::new();
+    let rocket_logging = Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "{}[{}]\t{}",
+                Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
+                record.target(),
+                message
+            ))
+        })
+        .level_for("rocket", LevelFilter::Info)
+        .chain(log_file("mugen.log").unwrap());
+
+    let stdout_logging = Dispatch::new()
+        .format(|out, message, record| {
+            if record.target().ends_with("_") {
+                out.finish(format_args!("\t{}", message));
+            } else {
+                out.finish(format_args!("{}", message));
+            }
+        })
+        .level_for("rocket", LevelFilter::Info)
+        .chain(std::io::stdout());
+
+    base_logging
+        .chain(rocket_logging)
+        .chain(stdout_logging)
+        .apply()
+        .unwrap();
+}
 
 #[tokio::main]
-async fn main() -> Result<(), rocket::Error> {
+async fn main() -> Result<(), RocketError> {
     dotenv().ok();
+
+    // Configure logger at runtime
+    setup_logging();
+
     let app: Rocket<Build> = app::configure();
 
     app.launch().await
