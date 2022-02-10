9c883566ec32dd3d49e75951c531266d795954ba
koopa1338 <sinner1991@gmail.com>
2022-02-10 14:08:09 +0100

    enabled all fields for doc model

--- mugen-server/src/models/document.rs
+++ mugen-server/src/models/document.rs
@@ -7,12 +7,12 @@ pub struct Model {
     #[sea_orm(primary_key)]
     #[serde(skip_deserializing)]
     pub id: i64,
-    // pub created: DateTime,
-    // pub last_updated: DateTime,
-    // pub filetype: String,
+    pub created: DateTime,
+    pub last_updated: DateTime,
+    pub filetype: String,
     pub version: u32,
-    // pub size: u64,
-    // pub data: Vec<u8>,
+    pub size: u64,
+    pub data: Vec<u8>,
 }
 
 #[derive(Copy, Clone, Debug, EnumIter)]
