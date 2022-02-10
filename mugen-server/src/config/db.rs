7e36fbf8e43970fd52008cb5227014b7bb82d58b
koopa1338 <sinner1991@gmail.com>
2022-02-10 12:09:33 +0100

    moved db connection to config::db module

--- mugen-server/src/config/db.rs
+++ mugen-server/src/config/db.rs
@@ -1,1 +1,16 @@
+use anyhow::Result;
+use std::time::Duration;
+use sea_orm::{ConnectOptions, Database, DatabaseConnection};
 
+use super::app;
+
+pub async fn get_database_connection_pool(config: app::Config) -> Result<DatabaseConnection> {
+    let mut db= ConnectOptions::new(config.database_url);
+    db.max_connections(100)
+        .min_connections(5)
+        .connect_timeout(Duration::from_secs(8))
+        .idle_timeout(Duration::from_secs(8))
+        .sqlx_logging(true);
+
+    Ok(Database::connect(db).await?)
+}
--- mugen-server/src/main.rs
+++ mugen-server/src/main.rs
@@ -4,11 +4,9 @@ mod handler;
 mod models;
 mod services;
 
-use std::time::Duration;
-
 use config::app;
+use config::db;
 use dotenv::dotenv;
-use sea_orm::{ConnectOptions, Database};
 
 use anyhow::Result;
 use clap::Parser;
@@ -19,18 +17,10 @@ async fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
 
     let config = app::Config::parse();
-
-    let mut db = ConnectOptions::new(config.database_url);
-    db.max_connections(100)
-        .min_connections(5)
-        .connect_timeout(Duration::from_secs(8))
-        .idle_timeout(Duration::from_secs(8))
-        .sqlx_logging(true);
-
-    let conn = Database::connect(db).await?;
+    let conn = db::get_database_connection_pool(config);
 
     let frontend = app::static_routes();
-    let backend = app::api_routes(conn);
+    let backend = app::api_routes(conn.await?);
 
     tokio::join!(frontend, backend);
 
