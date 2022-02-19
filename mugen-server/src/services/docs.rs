89664334f0d9a13348f43372fa604673f4f3f675
koopa1338 <sinner1991@gmail.com>
2022-02-19 11:34:30 +0100

    added tracing attributes to display called function names in logs

--- mugen-server/src/services/docs.rs
+++ mugen-server/src/services/docs.rs
@@ -1,10 +1,13 @@
 use entity::document::{Entity as Document, Model as DocumentModel};
 use sea_orm::{prelude::*, DatabaseConnection, DeleteResult, IntoActiveModel, Set};
+use tracing_attributes::instrument;
 
+#[instrument]
 pub async fn get_docs(conn: &DatabaseConnection) -> Result<Vec<DocumentModel>, DbErr> {
     Document::find().all(conn).await
 }
 
+#[instrument]
 pub async fn get_doc_by_id(id: i64, conn: &DatabaseConnection) -> Result<DocumentModel, DbErr> {
     Document::find_by_id(id)
         .one(conn)
@@ -12,6 +15,7 @@ pub async fn get_doc_by_id(id: i64, conn: &DatabaseConnection) -> Result<Documen
         .ok_or_else(|| DbErr::RecordNotFound(format!("No Document with id {id} found")))
 }
 
+#[instrument]
 pub async fn create_doc(
     data: DocumentModel,
     conn: &DatabaseConnection,
@@ -21,6 +25,7 @@ pub async fn create_doc(
         .await
 }
 
+#[instrument]
 pub async fn update_doc(
     data: DocumentModel,
     id: i64,
@@ -31,6 +36,7 @@ pub async fn update_doc(
     Document::update(active_model).exec(conn).await
 }
 
+#[instrument]
 pub async fn delete_doc(id: i64, conn: &DatabaseConnection) -> Result<DeleteResult, DbErr> {
     let document = get_doc_by_id(id, conn).await?;
 
