c3b6a452ed2424b5bdc55bdf5fba0b028a8ed032
koopa1338 <sinner1991@gmail.com>
2022-03-04 23:37:10 +0100

    wip: dynamic logging layers

--- mugen-server/src/utils/logging.rs
+++ mugen-server/src/utils/logging.rs
@@ -1,13 +1,19 @@
 #![allow(unused_macros)]
 
-use std::fs::{create_dir_all,read_to_string};
-use std::str::FromStr;
+use std::fmt::Debug;
+use std::fs::{create_dir_all, read_to_string};
+use std::ops::Deref;
 use std::path::PathBuf;
+use std::str::FromStr;
 
 use serde::Deserialize;
 
 use anyhow::Result;
 use dotenv::dotenv;
+use tracing::metadata::LevelFilter;
+use tracing::span::{Attributes, Record};
+use tracing::subscriber::Interest;
+use tracing::{Event, Id, Metadata, Subscriber};
 use tracing_appender::non_blocking::{NonBlocking, WorkerGuard};
 use tracing_subscriber::{
     filter::{Filtered, Targets},
@@ -20,7 +26,10 @@ use tracing_subscriber::{
 // #[serde(rename_all(deserialize = "lowercase"))]
 // pub enum LogFormat {
 //     Full,
-//     Compact, Pretty, Json, }
+//     Compact,
+//     Pretty,
+//     Json,
+// }
 
 #[derive(Debug, Deserialize)]
 pub struct FileLogger {
@@ -42,18 +51,11 @@ pub struct LogConfig {
     pub file_logger: Option<Vec<FileLogger>>,
 }
 
-macro_rules! register_layers {
-    () => {
-        todo!()
-    };
-}
-
 pub fn init_logging() -> Result<Vec<WorkerGuard>> {
     let conf: LogConfig = toml::from_str(&read_to_string(dotenv::var("LOGGING")?)?)?;
 
-    let mut layers: Vec<
-        Filtered<fmt::Layer<_, DefaultFields, Format, NonBlocking>, Targets, FmtSubscriber>,
-    > = Vec::new();
+    let mut layers: Vec<Filtered<fmt::Layer<_, DefaultFields, Format, _>, Targets, FmtSubscriber>> =
+        Vec::new();
     let mut guards = Vec::new();
 
     if let Some(file_logger_table) = conf.file_logger {
@@ -70,8 +72,10 @@ pub fn init_logging() -> Result<Vec<WorkerGuard>> {
                 guards.push(guard);
 
                 let file_targets = Targets::from_str(&entry.modules.join(",")).unwrap();
-                let file_layer = fmt::layer()
+                // TODO: call fn on layer with parsed format
+                let file_layer = fmt::Layer::new()
                     .with_writer(file_writer)
+                    .with_ansi(false)
                     .with_filter(file_targets);
 
                 layers.push(file_layer);
@@ -80,10 +84,16 @@ pub fn init_logging() -> Result<Vec<WorkerGuard>> {
 
     let targets = Targets::from_str(&conf.stream_logger.modules.join(","))?;
     // TODO: call fn on layer with parsed format
-    let stream_layer = fmt::layer()
+    let stream_layer = fmt::Layer::new()
         .with_writer(std::io::stdout)
+        .with_ansi(true)
         .with_filter(targets);
 
+    // TODO: how to register multiple layers on the same subsciber dynamically?
+    // already tried:
+    //      - combine layers with `and_then(layer)`
+    //      - concat `with(layer)` calls
+    //          - not possible because types of stdout and layer with file writers are different
     tracing_subscriber::registry().with(stream_layer).init();
 
     Ok(guards)
