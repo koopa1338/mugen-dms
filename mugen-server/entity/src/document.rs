f8954c7f7f3d911317c56d277a5e06aa5a3d1e0d
koopa1338 <sinner1991@gmail.com>
2022-07-09 02:53:50 +0200

    serde for data of documents
    deserialize the base64 encoded content into bytes and reverse

--- mugen-server/entity/src/document.rs
+++ mugen-server/entity/src/document.rs
@@ -1,10 +1,10 @@
 use sea_orm::entity::prelude::*;
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
 
 #[derive(Clone, Debug, PartialEq, DeriveEntityModel, Serialize, Deserialize)]
 #[sea_orm(table_name = "documents")]
 pub struct Model {
-    #[sea_orm(primary_key)]
+    #[sea_orm(primary_key, auto_increment = true)]
     #[serde(skip_deserializing)]
     pub id: i64,
     pub created: Option<DateTimeWithTimeZone>,
@@ -12,11 +12,30 @@ pub struct Model {
     pub filetype: String,
     pub version: i32,
     pub size: i64,
+    #[serde(deserialize_with = "deserialize_b64", serialize_with = "serialize_b64")]
     pub data: Option<Vec<u8>>,
+}
+
+fn deserialize_b64<'de, D>(deserializer: D) -> Result<Option<Vec<u8>>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let raw = String::deserialize(deserializer)?;
+    Ok(Some(raw.into_bytes()))
+}
+
+fn serialize_b64<'a, S>(bytes: &'a Option<Vec<u8>>, s: S) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+    if let Some(bytes) = bytes {
+        let base64 = std::str::from_utf8(&bytes).unwrap();
+        return s.serialize_some(&base64);
+    }
+    s.serialize_none()
 }
 
 #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
 pub enum Relation {}
 
 impl ActiveModelBehavior for ActiveModel {}
-
