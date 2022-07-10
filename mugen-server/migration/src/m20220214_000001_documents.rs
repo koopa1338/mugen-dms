0153ac3ee8a0ab4f976ea1829ae17fe670c48c9d
koopa <sinner1991@gmail.com>
2022-07-10 15:12:52 +0200

    updated sea orm and improved migrations (#17)

--- mugen-server/migration/src/m20220214_000001_documents.rs
+++ mugen-server/migration/src/m20220214_000001_documents.rs
@@ -1,5 +1,5 @@
-use entity::document::*;
-use sea_schema::migration::prelude::*;
+use entity::{sea_orm::Schema, document::Entity as Document};
+pub use sea_orm_migration::prelude::*;
 
 pub struct Migration;
 
@@ -12,46 +12,17 @@ impl MigrationName for Migration {
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+
+        let builder = manager.get_database_backend();
+        let schema = Schema::new(builder);
         manager
-            .create_table(
-                sea_query::Table::create()
-                    .table(Entity)
-                    .if_not_exists()
-                    .col(
-                        ColumnDef::new(Column::Id)
-                            .big_integer()
-                            .not_null()
-                            .auto_increment()
-                            .primary_key(),
-                    )
-                    .col(ColumnDef::new(Column::Created).timestamp_with_time_zone())
-                    .col(ColumnDef::new(Column::LastUpdated).timestamp_with_time_zone())
-                    .col(
-                        ColumnDef::new(Column::Filetype)
-                            .string()
-                            .default(String::from("unknown")),
-                    )
-                    .col(
-                        ColumnDef::new(Column::Version)
-                            .integer()
-                            .not_null()
-                            .default(1i32),
-                    )
-                    .col(
-                        ColumnDef::new(Column::Size)
-                            .big_integer()
-                            .not_null()
-                            .default(0i64),
-                    )
-                    .col(ColumnDef::new(Column::Data).binary())
-                    .to_owned(),
-            )
+            .create_table(schema.create_table_from_entity(Document))
             .await
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         manager
-            .drop_table(Table::drop().table(Entity).to_owned())
+            .drop_table(Table::drop().table(Document).to_owned())
             .await
     }
 }
