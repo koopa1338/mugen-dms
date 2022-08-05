1c97aea9ae09a196ae54308285e706c3dda9c559
koopa1338 <sinner1991@gmail.com>
2022-08-05 23:45:47 +0200

    fixed clippy return warning

--- mugen-server/src/utils/logging.rs
+++ mugen-server/src/utils/logging.rs
@@ -79,35 +79,32 @@ pub fn init_logging() -> Result<Vec<WorkerGuard>> {
             .for_each(|entry| {
                 let log_dir = &entry.path;
                 if !log_dir.exists() {
-                    if let Err(io) = create_dir_all(&log_dir) {
-                        eprintln!("Error creating logging dir {}.", &log_dir.display());
-                        return;
-                    }
-                }
-                let appender = tracing_appender::rolling::never(log_dir, &entry.filename);
-                let (file_writer, guard) = tracing_appender::non_blocking(appender);
-                guards.push(guard);
+                    if let Ok(io) = create_dir_all(&log_dir) {
+                        let appender = tracing_appender::rolling::never(log_dir, &entry.filename);
+                        let (file_writer, guard) = tracing_appender::non_blocking(appender);
+                        guards.push(guard);
 
-                match Targets::from_str(&entry.modules.join(",")) {
-                    Ok(file_targets) => {
-                        let file_layer =
-                            fmt::Layer::new().with_writer(file_writer).with_ansi(false);
-                        let layer = match entry.format {
-                            LogFormat::Full => file_layer.with_filter(file_targets).boxed(),
-                            LogFormat::Compact => {
-                                file_layer.compact().with_filter(file_targets).boxed()
-                            }
-                            LogFormat::Pretty => {
-                                file_layer.pretty().with_filter(file_targets).boxed()
-                            }
-                            LogFormat::Json => file_layer.json().with_filter(file_targets).boxed(),
-                        };
+                        match Targets::from_str(&entry.modules.join(",")) {
+                            Ok(file_targets) => {
+                                let file_layer =
+                                    fmt::Layer::new().with_writer(file_writer).with_ansi(false);
+                                let layer = match entry.format {
+                                    LogFormat::Full => file_layer.with_filter(file_targets).boxed(),
+                                    LogFormat::Compact => {
+                                        file_layer.compact().with_filter(file_targets).boxed()
+                                    }
+                                    LogFormat::Pretty => {
+                                        file_layer.pretty().with_filter(file_targets).boxed()
+                                    }
+                                    LogFormat::Json => file_layer.json().with_filter(file_targets).boxed(),
+                                };
 
-                        layers.push(layer);
-                    }
-                    Err(error) => {
-                        eprintln!("Error parsing file targets. failed to initialize file logging for {:#?}", entry);
-                        return;
+                                layers.push(layer);
+                            }
+                            Err(error) => {
+                                eprintln!("Error parsing file targets. failed to initialize file logging for {:#?}", entry);
+                            }
+                        }
                     }
                 }
             });
