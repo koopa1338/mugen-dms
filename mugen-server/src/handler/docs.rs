1b1388b0e36cfc40a9545711a323574024b7f86d
koopa1338 <sinner1991@gmail.com>
2022-09-25 16:57:18 +0200

    cleanup and inline service results in matches

--- mugen-server/src/handler/docs.rs
+++ mugen-server/src/handler/docs.rs
@@ -22,8 +22,7 @@ pub fn router() -> Router {
 pub async fn doc_list(
     Extension(ref conn): Extension<DatabaseConnection>,
 ) -> Result<Json<Vec<DocumentModel>>, (StatusCode, Json<DbErrJsonValue>)> {
-    let result = services::docs::get_docs(conn).await;
-    match result {
+    match services::docs::get_docs(conn).await {
         Ok(documents) => {
             debug!("Retrieved {} documents", documents.len());
             Ok(Json(documents))
@@ -37,9 +36,7 @@ pub async fn doc_by_id(
     Path(id): Path<i64>,
     Extension(ref conn): Extension<DatabaseConnection>,
 ) -> Result<Json<DocumentModel>, (StatusCode, Json<DbErrJsonValue>)> {
-    //TODO: make this a function that takes the service call as a Fn
-    let result = services::docs::get_doc_by_id(id, conn).await;
-    match result {
+    match services::docs::get_doc_by_id(id, conn).await {
         Ok(document) => {
             debug!("Retrieved document with id {}", document.id);
             trace!("{document}");
@@ -71,8 +68,7 @@ pub async fn doc_update(
     Json(input): Json<DocumentModel>,
     Extension(ref conn): Extension<DatabaseConnection>,
 ) -> Result<Json<DocumentModel>, (StatusCode, Json<DbErrJsonValue>)> {
-    let result = services::docs::update_doc(input, id, conn).await;
-    match result {
+    match services::docs::update_doc(input, id, conn).await {
         Ok(document) => {
             debug!("Document with id {} was updated", document.id);
             trace!("New data {document}");
@@ -87,11 +83,10 @@ pub async fn doc_delete(
     Path(id): Path<i64>,
     Extension(ref conn): Extension<DatabaseConnection>,
 ) -> Result<Json<u64>, (StatusCode, Json<DbErrJsonValue>)> {
-    let result = services::docs::delete_doc(id, conn).await;
-    match result {
+    match services::docs::delete_doc(id, conn).await {
         Ok(document) => {
             debug!("deleted document with id {id}");
-            //TODO: we might want to respond with useful json
+            // TODO: we might want to respond with useful json
             Ok(Json(document.rows_affected))
         }
         Err(dberror) => Err((StatusCode::INTERNAL_SERVER_ERROR, Json(dberror.into()))),
