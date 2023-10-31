2ef558334603103284abee3de70f295e1599be28
koopa1338 <sinner1991@gmail.com>
2023-10-31 02:10:52 +0100

    sidebar components

---
+++ frontend/src/lib.rs
@@ -0,0 +1,26 @@
+use leptos::*;
+
+#[derive(Debug, Copy, Clone)]
+pub struct ToggleSignal {
+    inner: RwSignal<bool>,
+}
+
+impl ToggleSignal {
+    pub fn new(value: bool) -> Self {
+        Self {
+            inner: create_rw_signal(value),
+        }
+    }
+
+    pub fn toggle(&mut self) {
+        self.inner.update(|inner| *inner = !*inner);
+    }
+
+    pub fn get(&self) -> bool {
+        self.inner.get()
+    }
+
+    pub fn set(&self, value: bool) {
+        self.inner.set(value);
+    }
+}
