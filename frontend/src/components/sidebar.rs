59a3bd082bd9bcd9f4fd608fe0b6a03b191c76ac
koopa1338 <sinner1991@gmail.com>
2023-10-31 23:06:32 +0100

    remove dropdown menu because auf transition impl issues

--- frontend/src/components/sidebar.rs
+++ frontend/src/components/sidebar.rs
@@ -1,24 +1,25 @@
 use leptos::*;
 use leptos_router::*;
-use mugen_frontend::ToggleSignal;
+// use mugen_frontend::ToggleSignal;
 
 #[component]
 pub fn Sidebar() -> impl IntoView {
     view! {
         <nav class="sidebar">
             <ul>
-                <MultiMenu label={"Home"}>
-                    <SingleMenu href="/kekw" label="kekw" />
-                    <SingleMenu href="/kekl" label="kekl" />
-                    <SingleMenu href="/lul" label="lul" />
-                    <SingleMenu href="/wat" label="wat" />
-                </MultiMenu>
-                <SingleMenu href="/nononono" label="trolololo" />
+                <SingleMenu href="/kekw" label="kekw"/>
+                <SingleMenu href="/kekl" label="kekl"/>
+                <SingleMenu href="/lul" label="lul"/>
+                <SingleMenu href="/wat" label="wat"/>
+                <SingleMenu href="/nononono" label="trolololo"/>
             </ul>
         </nav>
     }
 }
 
+/*
+TODO: find a way to make the transition of collapsibles only with tailwind css or a feasable
+workaround
 #[component]
 pub fn MultiMenu(#[prop(optional)] label: &'static str, children: Children) -> impl IntoView {
     let mut collapsed = ToggleSignal::new(false);
@@ -34,12 +35,17 @@ pub fn MultiMenu(#[prop(optional)] label: &'static str, children: Children) -> i
     view! {
         <li>
             <a on:click=toggle>{label}</a>
-            <ul class="transition-all" class=("hidden", move || !collapsed.get())>
+            <ul class=move || if !collapsed.get() {
+                    "transition-all duration-200 ease-in-out max-h-0 opacity-0"
+                } else {
+                    "transition-all duration-300 ease-in max-h-none opacity-100"
+                } >
                 {children()}
             </ul>
         </li>
     }
 }
+*/
 
 #[component]
 pub fn SingleMenu<H: ToHref + 'static>(
@@ -48,9 +54,7 @@ pub fn SingleMenu<H: ToHref + 'static>(
 ) -> impl IntoView {
     view! {
         <li>
-            <A href=href>
-                {label}
-            </A>
+            <A href=href>{label}</A>
         </li>
     }
 }
