1698c9e57813019f522ddb6ec696df9e67a5636d
koopa1338 <sinner1991@gmail.com>
2023-11-05 23:30:21 +0100

    error handling and rendering document table

--- frontend/src/api.rs
+++ frontend/src/api.rs
@@ -1,13 +1,16 @@
 use gloo_net::http::{Method, RequestBuilder};
 use leptos::Serializable;
 use serde::{Deserialize, Serialize};
+use thiserror::Error;
+
+use gloo_utils::errors::JsError;
 
 pub async fn api_call<T, P>(
     path: &str,
     method: Method,
     payload: Option<P>,
     query_params: Option<Vec<(&str, &str)>>,
-) -> Option<T>
+) -> Result<T, FetchError>
 where
     T: Serializable, // this means both serializable and deserializable
     P: Serialize,
@@ -22,30 +25,48 @@ where
         }
     });
 
-    let mut builder = RequestBuilder::new(path)
+    let mut builder = RequestBuilder::new(&format!("http://localhost:4000/{path}"))
         .method(method)
         .abort_signal(abort_signal.as_ref());
     if let Some(params) = query_params {
         builder = builder.query(params);
     }
 
+    // TODO: map errors to suitable error messages for frontend
     let req = if let Some(payload) = payload {
         builder
             .json(&payload)
-            .map_err(|e| log::error!("{e}"))
-            .ok()?
+            .map_err(|e| FetchError::PayloadError(e.to_string()))?
     } else {
-        builder.build().map_err(|e| log::error!("{e}")).ok()?
+        builder
+            .build()
+            .map_err(|e| FetchError::ClientError(e.to_string()))?
     };
 
     let json = req
         .send()
         .await
-        .map_err(|e| log::error!("{e}"))
-        .ok()?
+        .map_err(|e| {
+            let error_message = match e {
+                gloo_net::Error::JsError(JsError { message, .. }) => message,
+                gloo_net::Error::SerdeError(e) => e.to_string(),
+                gloo_net::Error::GlooError(msg) => msg,
+            };
+            FetchError::GenericError(error_message)
+        })?
         .text()
         .await
-        .ok()?;
+        .map_err(|e| FetchError::GenericError(e.to_string()))?;
 
-    T::de(&json).ok()
+    T::de(&json).map_err(|e| FetchError::GenericError(e.to_string()))
+}
+
+#[derive(Error, Clone, Debug, Deserialize, Serialize)]
+pub enum FetchError {
+    #[error("Generic Error: {0}")]
+    GenericError(String),
+    #[error("Error initializing request client: {0}")]
+    ClientError(String),
+    #[error("Error setting payload: {0}")]
+    PayloadError(String),
 }
