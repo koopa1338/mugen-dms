8f51e35fc9960fb651f36b3f536ab19a83d4b1e1
koopa1338 <sinner1991@gmail.com>
2023-11-06 22:50:26 +0100

    add upload component with drag and drop

--- frontend/src/routes/home.rs
+++ frontend/src/routes/home.rs
@@ -1,11 +1,19 @@
 use leptos::*;
+use web_sys::FileList;
+
+use crate::components::upload::Upload;
 
 #[component]
 pub(crate) fn Home() -> impl IntoView {
+    let upload_callback = move |file_list: FileList| {
+        logging::warn!("Number of uploaded files: {}", file_list.length());
+    };
     view! {
         <div class="grid grid-cols-1 gap-4 mb-4">
             <div class="flex items-center justify-center h-24 rounded bg-gray-900">
-                <p class="text-2xl text-gray-500">TODO</p>
+                <Upload callback=upload_callback multiple=true>
+                    "Drag and drop files here or click for file dialogue"
+                </Upload>
             </div>
         </div>
     }
