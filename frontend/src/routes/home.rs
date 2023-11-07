81736211a730cca620d5b51f022bf9faeb4d1ea8
koopa1338 <sinner1991@gmail.com>
2023-11-07 01:07:33 +0100

    refactored upload component
    the input is not used anymore and I added a button and a label text for
    the component. Furthermore the callback is not optional anymore because
    the component itself has no knowledge what it should do with the files
    selected, so the callback is indeed required. Also the drag and click
    events do not trigger a click event on the input element itself, this
    caused recursive error in the console on the client so the open picker
    is not triggered directly.

--- frontend/src/routes/home.rs
+++ frontend/src/routes/home.rs
@@ -7,14 +7,17 @@ use crate::components::upload::Upload;
 pub(crate) fn Home() -> impl IntoView {
     let upload_callback = move |file_list: FileList| {
         logging::warn!("Number of uploaded files: {}", file_list.length());
+        for idx in 0..file_list.length() {
+            logging::warn!(
+                "filename: {:?}",
+                file_list
+                    .item(idx)
+                    .map(|file| file.name())
+                    .unwrap()
+            );
+        }
     };
     view! {
-        <div class="grid grid-cols-1 gap-4 mb-4">
-            <div class="flex items-center justify-center h-24 rounded bg-gray-900">
-                <Upload callback=upload_callback multiple=true>
-                    "Drag and drop files here or click for file dialogue"
-                </Upload>
-            </div>
-        </div>
+        <Upload callback=upload_callback multiple=true/>
     }
 }
