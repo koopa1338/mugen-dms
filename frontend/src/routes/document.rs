158da2cc8cfac22f46452eb138f255b2b9ecbd39
koopa1338 <sinner1991@gmail.com>
2023-11-09 02:21:09 +0100

    wip: infinite scroll for document table

--- frontend/src/routes/document.rs
+++ frontend/src/routes/document.rs
@@ -1,42 +1,60 @@
 use common::models::document::Doc;
 use gloo_net::http::Method;
 use leptos::*;
+use leptos_use::{use_infinite_scroll_with_options, UseInfiniteScrollOptions};
 
 use crate::api::{api_call, FetchError};
 use crate::ChronoFormat;
 
 #[component]
 pub(crate) fn Documents() -> impl IntoView {
+    let el = create_node_ref::<html::Tbody>();
+    let (page, page_set) = create_signal(1);
+    let (data, set_data) = create_signal(vec![]);
+
     let documents = create_resource(
-        move || (),
-        move |_| async move {
-            api_call::<Vec<Doc>, FetchError>("api/doc", Method::GET, None, None).await
+        move || page.get(),
+        move |page| async move {
+            api_call::<Vec<Doc>, FetchError>(
+                "api/doc",
+                Method::GET,
+                None,
+                Some(vec![("page", &page.to_string()), ("page_size", "1")]),
+            )
+            .await
         },
     );
 
+    // FIXME: this keeps fetching the api and doesn't stop when elements reach the end of the
+    // viewport
+    let _ = use_infinite_scroll_with_options(
+        el,
+        move |_| async move {
+            page_set.update(|page| *page += 1);
+            documents.and_then(|k| set_data.update(|d| d.extend_from_slice(&k.clone())));
+        },
+        UseInfiniteScrollOptions::default().distance(200.0),
+    );
+
     let documents_view = move || {
-        documents.get().map(|doc| {
-            doc.map(|doc| {
-                view! {
-                    <tbody>
-                        <For each=move || doc.clone() key=|doc| doc.id let:doc>
-                            <tr>
-                                <th class="py-2">{doc.id}</th>
-                                <th class="py-2">{doc.version}</th>
-                                <th class="py-2">{doc.created.display()}</th>
-                                <th class="py-2">
-                                    {doc
-                                        .updated
-                                        .map(|date| date.display())
-                                        .unwrap_or("never".to_string())}
-                                </th>
-                                <th class="py-2">{doc.filetype}</th>
-                            </tr>
-                        </For>
-                    </tbody>
-                }
-            })
-        })
+        view! {
+            <tbody node_ref=el>
+                <For each=move || data.get().clone() key=|doc| doc.id let:doc>
+                    <tr>
+                        <th class="py-2">{doc.id}</th>
+                        <th class="py-2">{doc.version}</th>
+                        <th class="py-2">{doc.created.display()}</th>
+                        <th class="py-2">
+                            {doc
+                                .updated
+                                .map(|date| date.display())
+                                .unwrap_or("never".to_string())}
+                        </th>
+                        <th class="py-2">{doc.filetype}</th>
+                    </tr>
+                </For>
+            </tbody>
+        }
     };
 
     view! {
