8c89a4f442e6a61a5aeec62e238ef292caa0cb51
koopa1338 <sinner1991@gmail.com>
2024-01-27 02:00:08 +0100

    removed charming charts

--- frontend/src/components/mod.rs
+++ frontend/src/components/mod.rs
@@ -1,4 +1,3 @@
-pub mod chart;
 pub mod grid;
 pub mod sidebar;
 pub mod upload;
--- frontend/src/routes/dashboard.rs
+++ frontend/src/routes/dashboard.rs
@@ -1,14 +1,7 @@
-use charming::{
-    component::{self, *},
-    df,
-    element::{self, *},
-    series::{self, *},
-    Animation as ResizeAnimation, Chart, ChartResize, Easing, WasmRenderer,
-};
 use gloo_file::{futures, FileList};
 use leptos::{callback::Callback, *};
 
-use crate::components::{chart::Chart, grid::Grid, upload::Upload};
+use crate::components::{grid::Grid, upload::Upload};
 
 #[component]
 pub(crate) fn Dashboard() -> impl IntoView {
@@ -21,20 +14,8 @@ pub(crate) fn Dashboard() -> impl IntoView {
 
     let upload_callback = Callback::new(upload_closure);
 
-    // testing chart
-    let chart = Chart::new()
-        .title(Title::new().text("Demo: Leptos + Charming"))
-        .x_axis(
-            Axis::new()
-                .type_(AxisType::Category)
-                .data(vec!["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]),
-        )
-        .y_axis(Axis::new().type_(AxisType::Value))
-        .series(Line::new().data(vec![150, 230, 224, 218, 135, 147, 260]));
-
     view! {
         <Grid classes="grid-cols-2 mb-4 h-full">
-            <Chart chart=chart id="chart-test"/>
             <Upload callback=upload_callback multiple=true accept="application/pdf"/>
         </Grid>
     }
